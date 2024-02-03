267d600906cabd9e8e18f369c7f9e16abe031dba
Vaibhav Jain <113410574+Vaibhav12708@users.noreply.github.com>
2024-02-03 16:54:16 +0530

    Largest Prime Factor

--- Python/Problem 3.py
+++ Python/Problem 3.py
@@ -1,3 +1,6 @@
+##static code
+#Example 1
+
 def largest_prime_factor(number):
     factor = 2
     while factor * factor <= number:
@@ -12,3 +15,22 @@ def largest_prime_factor(number):
 number = 600851475143
 largest_prime = largest_prime_factor(number)
 print(largest_prime)
+
+
+
+
+
+## dynamic code
+#Example 2
+
+import math
+n=int(input())
+list1=[]
+while n%2==0:
+	list1.append(2)
+	n=n/2
+for i in range(3,int(math.sqrt(n))+1,2):
+	while n%i==0:
+		list1.append(i)
+		n=n/i
+print(list1)
